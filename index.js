c2e13cc252c772ba0d0763892c35b412fbacd080
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-07-17 16:53:47 +0530

    added vercel frontend url

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chat-server-flame.vercel.app/",
+    origin: "https://chat-frontend-pied.vercel.app/",
   },
 });
 
